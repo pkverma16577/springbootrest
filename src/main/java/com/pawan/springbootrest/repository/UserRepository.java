e4b487d7fd7b934f28d9d6d709957b0d58a2fc27
Pawan <pkverma16577@gmail.com>
2024-01-16 15:06:22 +0530

    changed in repository

--- src/main/java/com/pawan/springbootrest/repository/UserRepository.java
+++ src/main/java/com/pawan/springbootrest/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.pawan.springbootrest.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pawan.springbootrest.model.User;
-
+// here we don't require @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
